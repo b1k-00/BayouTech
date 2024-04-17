6f894d3428749ad2ccad11e0a3e0defa01208a2f
BC0LL1N5 <brennencollins44@gmail.com>
2024-04-17 12:14:48 -0500

    dbCOntext updates

---
+++ BayouTech/Persistence/BayouTechDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BayouTech.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BayouTech.Persistence
+{
+    public class BayouTechDbContext: DbContext
+    {
+        public BayouTechDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+        }
+
+        public DbSet<Game> Games { get; set; }
+
+        public DbSet<Team> Teams { get; set; }
+
+        public DbSet<Player> Players { get; set; }
+
+
+    }
+}
